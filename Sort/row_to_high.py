da65e7365cdb5eaeb064240a2ef8c3007cba95ba
JayPark0110 <90092440+JayPark0110@users.noreply.github.com>
2022-01-28 10:35:35 +0900

    sort 2

---
+++ Sort/row_to_high.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+array = []
+for i in range(n):
+    input_data = input().split()
+    array.append((input_data[0], int(input_data[1])))
+
+array = sorted(array, key=lambda std: std[1])
+
+for i in array:
+    print(i[0], end=' ')
