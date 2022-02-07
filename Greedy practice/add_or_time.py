7f3f70ee04510329fb836d78ad6eb4dbeea26f5f
regularPark <90092440+JayPark0110@users.noreply.github.com>
2022-02-07 17:09:52 +0900

    그리디 문제 풀이

--- Greedy practice/add_or_time.py
+++ Greedy practice/add_or_time.py
@@ -1,3 +1,7 @@
+# 연속된 숫자열을 순차적으로 돌면서 덧셈, 혹은 곱셈을 통해 가장 큰 수를 만들어 내는 프로그램을 만들라.
+# ex) input: 567, output : 210
+# ex) input: 02984, output : 576
+
 numbers = input()
 result = int(numbers[0])
 for i in range(1, len(numbers)):
