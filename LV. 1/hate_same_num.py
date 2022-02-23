eec590f381ded60877e67c7ab1d4018a41562ae2
regularPark <90092440+JayPark0110@users.noreply.github.com>
2022-02-23 21:40:56 +0900

    lv1

--- LV. 1/hate_same_num.py
+++ LV. 1/hate_same_num.py
@@ -1,11 +1,23 @@
-def solution(arr):
-    answer = []
-    j = -1
-    for i in arr:
-        if i != j:
-            j = i
-            answer.append(j)
-    return answer
+# def solution(arr):
+#     answer = []
+#     j = -1
+#     for i in arr:
+#         if i != j:
+#             j = i
+#             answer.append(j)
+#     return answer
 
 
-print(solution([1, 1, 3, 3, 0, 1, 1]))
+# print(solution([1, 1, 3, 3, 0, 1, 1]))
+
+
+def no_continuous(s):
+    a = []
+    for i in s:
+        if a[-1:] == [i]:
+            continue
+        a.append(i)
+    return a
+
+
+print(no_continuous([1, 1, 3, 3, 0, 1, 1]))
