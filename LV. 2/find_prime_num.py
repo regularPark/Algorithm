2e74e97207d6641083e759b290f3e52d48221e55
JayPark0110 <90092440+JayPark0110@users.noreply.github.com>
2022-03-02 22:10:53 +0900

    lv2

--- LV. 2/find_prime_num.py
+++ LV. 2/find_prime_num.py
@@ -1,6 +1,8 @@
 from itertools import permutations
+from math import sqrt
 
 
+# my code
 def solution(numbers):
     res = []
     res2 = []
@@ -16,12 +18,31 @@ def solution(numbers):
             pn.append(int(rs2[_]))
     pn = set(pn)  # 정수화 && 중복 원소 제거
     for x in pn:
-        if x > 1:
-            for i in range(2, x):
+        if x == 2 or x == 3:
+            answer += 1
+        if x >= 4:
+            for i in range(2, int(sqrt(x)+1), 1):
                 if x % i == 0:
-                    continue
+                    break
+                if i == int(sqrt(x)):
+                    answer += 1
+                    break
 
     return answer
 
 
+# short code
+def solution(n):
+    a = set()
+    for i in range(len(n)):
+        a |= set(map(int, map("".join, permutations(list(n), i + 1))))
+    a -= set(range(0, 2))   # 0~1 원소를 삭제함
+    for i in range(2, int(max(a) ** 0.5) + 1):
+        a -= set(range(i * 2, max(a) + 1, i))   # i * 2의 배수들을 삭제하는 과정
+    return len(a)
+
+
 print(solution("011"))
+
+
+# recursion
