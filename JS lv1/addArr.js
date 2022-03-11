a0fc113f2b637a46575c668bdb21e806b002f4dd
regularPark <90092440+JayPark0110@users.noreply.github.com>
2022-03-11 17:23:43 +0900

    js lv1

--- JS lv1/addArr.js
+++ JS lv1/addArr.js
@@ -1,26 +1,27 @@
-// function solution(arr1, arr2) {
-//   var answer = [[]];
-//   for (var i = 0; i < arr1.length; i++) {
-//     answer[i] = [];
-//     for (var j = 0; j < arr1[0].length; j++) {
-//       answer[i][j] = arr1[i][j] + arr2[i][j];
-//     }
-//   }
-//   return answer;
-// }
+// my code
+function solution(arr1, arr2) {
+  var answer = [[]];
+  for (var i = 0; i < arr1.length; i++) {
+    answer[i] = [];
+    for (var j = 0; j < arr1[0].length; j++) {
+      answer[i][j] = arr1[i][j] + arr2[i][j];
+    }
+  }
+  return answer;
+}
 
-// console.log(
-//   solution(
-//     [
-//       [1, 2],
-//       [2, 3],
-//     ],
-//     [
-//       [3, 4],
-//       [5, 6],
-//     ]
-//   )
-// );
+console.log(
+  solution(
+    [
+      [1, 2],
+      [2, 3],
+    ],
+    [
+      [3, 4],
+      [5, 6],
+    ]
+  )
+);
 
 //short code
 function sumMatrix(A, B) {
